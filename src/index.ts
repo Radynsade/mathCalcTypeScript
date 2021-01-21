9274da548e533acffcfd81625da237d4746903bf
radynje <radynje@gmail.com>
2021-01-21 02:43:52 +0200

    Increase method

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,10 @@ export const transpose = (matrix: Matrix): Matrix => matrix.map((row, i, array) 
 
 export const negative = (matrix: Matrix): Matrix => matrix.map(row => row.map(num => -num));
 
+export const increase = (matrix: Matrix, times: number) => matrix.map(row => row.map(num => num * times));
+
+export const multiply = (...matrices: Matrix[]): Matrix =>
+
 const dropIndex = <T>(indexToDrop: number) => <T>(value: T, index: number): boolean => index !== indexToDrop;
 
 export const minor = (
@@ -32,6 +36,10 @@ export const adjunct = (
 	crossRow: number,
 	crossCol: number
 ): number => (-1) ** (crossCol + crossRow + 2) * determinant(minor(matrix, crossRow, crossCol));
+
+// export const inverse = (matrix: Matrix): Matrix | null => determinant(matrix) == 0 ? null : matrix.map(
+// 	(row: number[], i) => row.map((num: number, j) => )
+// );
 
 const matrix1: Matrix = [
 	[1, 2, -4],
